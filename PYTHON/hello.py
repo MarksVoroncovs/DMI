6a9859c64e263ed8e4404fe357e87f234d911084
saulespuke101@gmail.com <Mark-Voroncov@mail.ru>
2017-11-09 16:34:01 +0200

    20171026_17_55

---
+++ PYTHON/hello.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+
+print "Hello World!"
+
+
+
+#https://www.tutorialspoint.com/unix_commands/echo.htm
+# Example 3.-7.
+
+# example-3:
+print 'Here \bthe \bspaces \bare \bbackspaced.'
+
+
+# example-4:
+print 'Here \nthe \nspaces \nare \nnewlined.'
+
+
+# example-5:
+print 'Here \tthe \tspaces \thave \thorizontal \ttab \tspaces.'
+
+
+# example-6:
+print ''Here \vthe \vspaces \vhave \vvertical \vtab \vspaces.''
+
+
+# example-7:
+print 'Here the \rcontent before this is removed.' 
+
+
+
+print "Ja \ves \vvaretu \vEs \vpasauli \vrokas \v\b\vsanemtu"
+
+
+
---
+++ PYTHON/variables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+
+#https://www.tutorialspoint.com/unix_commands/echo.htm
+
+a = 65
+print type(a)
+print "Mainīga a vērtība kā dec skaitlis: %d"%(a)
+print "Mainīga a vērtība kā her skaitlis: %x"%(a)
+print "Mainīga a vērtība kā oct skaitlis: %o"%(a)
+print "Mainīga a vērtība kā  simbols: %c"%(a)
+
+
+
+
+
+
+a = 'A'; print type(a)
+print "Mainīga a vērtība kā dec skaitlis: %d"%(ord(a))
+print "Mainīga a vērtība kā her skaitlis: %x"%(ord(a))
+print "Mainīga a vērtība kā oct skaitlis: %o"%(ord(a))
+print "Mainīga a vērtība kā  simbols: %c"%(a)
