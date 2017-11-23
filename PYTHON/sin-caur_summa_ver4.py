52635f3b29570e82cd29fedb3b6dd82e393c6b86
saulespuke101@gmail.com <Mark-Voroncov@mail.ru>
2017-11-23 17:56:54 +0200

    20171123_17_56

---
+++ PYTHON/sin-caur_summa_ver4.py
@@ -0,0 +1,13 @@
+Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+ļietotāji, lūdzu, ievadi argumentu (x): 2
+<type 'float'>
+sin(2.00)=0.91
+a0 =   2.00 S0 =   2.00
+a1 =  -1.33 S1 =   0.67
+a2 =   0.27 S2 =   0.93
+a3 =  -0.03 S3 =   0.93
+>>> 
